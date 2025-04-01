65a6895c74abc60d4c6d434a4e18d934120bc738
Christian Johnson <chrjohnson@hmc.edu>
2025-04-01 16:21:58 -0700

    updates...

---
+++ src/homework18.py
@@ -0,0 +1,17 @@
+# %% [markdown]
+# **Homework 18**
+
+# %% [markdown]
+# Hard code your answers here and upload this document as homework18.py to Gradescope.
+
+# %%
+final_mse=33.019718170166016
+
+# %%
+class1_prob=0.32666666666666666
+class2_prob=0.34
+
+# %%
+accuracy=0.9800
+
+
